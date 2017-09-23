da2df199968fdecd723bf2a445e07ce82920924b
colinporth <colinporth@users.noreply.github.com>
2017-09-23 19:21:32 +0100

    add utils

--- utils/utils.h
+++ utils/utils.h
@@ -69,6 +69,21 @@ template <typename T> std::string dec (T value, uint16_t width = 0, char fill = 
 //}}}
 
 //{{{
+std::string getMsTimeStr (int bst, uint32_t msTime) {
+
+  // convert msTime to str
+  int subSec = msTime % 1000;
+  msTime /= 1000;
+  int second = msTime % 60;
+  msTime /= 60;
+  int minute = msTime  % 60;
+  msTime /= 60;
+  int hour = bst + msTime;
+
+  return dec(hour,2,'0') + "."  + dec(minute,2, '0') + "." + dec(second,2, '0') + "." + dec(subSec,3, '0');
+  }
+//}}}
+//{{{
 std::string getPtsStr (uint64_t pts) {
 
   pts /= 900;
