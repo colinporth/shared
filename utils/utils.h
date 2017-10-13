d42774373ea361354fd5b4609d7e243d7f019250
colinporth <colinporth@users.noreply.github.com>
2017-10-13 09:53:50 +0100

    fiddle with utils

--- utils/utils.h
+++ utils/utils.h
@@ -21,26 +21,6 @@
   #define smallFree             debugFree
   #define debug(str)            std::cout << str << std::endl
 
-  static double performanceFrequency = 0.0;
-  static __int64 performanceCounterStart = 0;
-  //{{{
-  inline double startTimer() {
-    LARGE_INTEGER largeInteger;
-    QueryPerformanceFrequency (&largeInteger);
-    performanceFrequency = double (largeInteger.QuadPart);
-    QueryPerformanceCounter (&largeInteger);
-    performanceCounterStart = largeInteger.QuadPart;
-    return double (largeInteger.QuadPart - performanceCounterStart) / performanceFrequency;
-    }
-  //}}}
-  //{{{
-  inline double getTimer() {
-    LARGE_INTEGER largeInteger;
-    QueryPerformanceCounter (&largeInteger);
-    return double (largeInteger.QuadPart - performanceCounterStart) / performanceFrequency;
-    }
-  //}}}
-
 #elif STM32
   #include "cLcd.h"
   #define bigMalloc(size,tag)    pvPortMalloc(size)
