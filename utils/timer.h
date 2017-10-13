188c7aeba96b3fcf2e02eacf65e0b67b5b40c31c
colinporth <colinporth@users.noreply.github.com>
2017-10-13 10:08:20 +0100

    simpler gettimer

--- utils/timer.h
+++ utils/timer.h
@@ -1,20 +1,10 @@
-// utils.h
+// timer.h
 #pragma once
-
-static double performanceFrequency = 0.0;
-static __int64 performanceCounterStart = 0;
-
-inline double startTimer() {
-  LARGE_INTEGER largeInteger;
-  QueryPerformanceFrequency (&largeInteger);
-  performanceFrequency = double (largeInteger.QuadPart);
-  QueryPerformanceCounter (&largeInteger);
-  performanceCounterStart = largeInteger.QuadPart;
-  return double (largeInteger.QuadPart - performanceCounterStart) / performanceFrequency;
-  }
 
 inline double getTimer() {
   LARGE_INTEGER largeInteger;
+  QueryPerformanceFrequency (&largeInteger);
+  auto performanceFrequency = double (largeInteger.QuadPart);
   QueryPerformanceCounter (&largeInteger);
-  return double (largeInteger.QuadPart - performanceCounterStart) / performanceFrequency;
+  return double (largeInteger.QuadPart) / performanceFrequency;
   }
