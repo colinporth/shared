0acf202958e5ee047f54feec9833b53af12ca3eb
colinporth <colinporth@users.noreply.github.com>
2017-10-29 13:33:04 +0000

    keep up to date

--- hls/hlsWidgets.h
+++ hls/hlsWidgets.h
@@ -296,12 +296,12 @@ private:
 //{{{
 void hlsMenu (cRootContainer* root, cHls* hls) {
 
-  root->addTopLeft (new cDecodePicWidget (r1x80, sizeof(r1x80), 3, 3, 1, hls->mHlsChan, hls->mChanChanged));
-  root->add (new cDecodePicWidget (r2x80, sizeof(r2x80), 3, 3, 2, hls->mHlsChan,  hls->mChanChanged));
-  root->add (new cDecodePicWidget (r3x80, sizeof(r3x80), 3, 3, 3, hls->mHlsChan,  hls->mChanChanged));
-  root->add (new cDecodePicWidget (r4x80, sizeof(r4x80), 3, 3, 4, hls->mHlsChan,  hls->mChanChanged));
-  root->add (new cDecodePicWidget (r5x80, sizeof(r5x80), 3, 3, 5, hls->mHlsChan,  hls->mChanChanged));
-  root->add (new cDecodePicWidget (r6x80, sizeof(r6x80), 3, 3, 6, hls->mHlsChan,  hls->mChanChanged));
+  root->addTopLeft (new cDecodePicWidget (r1x80, sizeof(r1x80), 3, 3, 1, hls->mChan, hls->mChanChanged));
+  root->add (new cDecodePicWidget (r2x80, sizeof(r2x80), 3, 3, 2, hls->mChan,  hls->mChanChanged));
+  root->add (new cDecodePicWidget (r3x80, sizeof(r3x80), 3, 3, 3, hls->mChan,  hls->mChanChanged));
+  root->add (new cDecodePicWidget (r4x80, sizeof(r4x80), 3, 3, 4, hls->mChan,  hls->mChanChanged));
+  root->add (new cDecodePicWidget (r5x80, sizeof(r5x80), 3, 3, 5, hls->mChan,  hls->mChanChanged));
+  root->add (new cDecodePicWidget (r6x80, sizeof(r6x80), 3, 3, 6, hls->mChan,  hls->mChanChanged));
 
   //root->add (new cHlsPicWidget (hls, 16/3.0f, 3));
   root->add (new cHlsDotsBox (hls, 1, 3));
