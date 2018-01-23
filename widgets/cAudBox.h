8ee82cf93633f942fa2b6ba66a01295e5401410b
colinporth <colinporth@users.noreply.github.com>
2018-01-23 12:13:39 +0000

    simpler

--- widgets/cAudBox.h
+++ widgets/cAudBox.h
@@ -7,7 +7,7 @@
 
 #include "../utils/utils.h"
 //}}}
-const float kBarsScale = 3.0f;  // need to work out power scaling derivation
+const float kBarsScale = 3.0f;  
 
 class cAudBox : public cWidget {
 public:
@@ -19,33 +19,26 @@ public:
   void onDraw (iDraw* draw) {
 
     if (mChans == 2) {
-      drawChanBar (draw, mX + 1,            mY, (mWidth/2)-1, mHeight, mPower[0]);
-      drawChanBar (draw, mX + 1 + mWidth/2, mY, (mWidth/2)-1, mHeight, mPower[1]);
+      drawBar (draw, 0xC000FF00, mX + 1,              mY, (mWidth/2)-1, mHeight, mPower[0]);
+      drawBar (draw, 0xC000FF00, mX + 1 + mWidth/2,   mY, (mWidth/2)-1, mHeight, mPower[1]);
       }
     else {
-      drawChanBar (draw, mX + 1,              mY, (mWidth/5)-1, mHeight, mPower[4]);
-      drawChanBar (draw, mX + 1 + mWidth/5,   mY, (mWidth/5)-1, mHeight, mPower[0]);
-      drawChanBar (draw, mX + 1 + mWidth*2/5, mY, (mWidth/5)-1, mHeight, mPower[2]);
-      drawChanBar (draw, mX + 1 + mWidth*3/5, mY, (mWidth/5)-1, mHeight, mPower[1]);
-      drawChanBar (draw, mX + 1 + mWidth*4/5, mY, (mWidth/5)-1, mHeight, mPower[5]);
-      drawWooferBar (draw, mX + 1, mY, mWidth-2, mHeight, mPower[3]);
+      drawBar (draw, 0xC000FF00, mX + 1,              mY, (mWidth/5)-1, mHeight, mPower[4]);
+      drawBar (draw, 0xC000FF00, mX + 1 + mWidth/5,   mY, (mWidth/5)-1, mHeight, mPower[0]);
+      drawBar (draw, 0xC000FF00, mX + 1 + mWidth*2/5, mY, (mWidth/5)-1, mHeight, mPower[2]);
+      drawBar (draw, 0xC000FF00, mX + 1 + mWidth*3/5, mY, (mWidth/5)-1, mHeight, mPower[1]);
+      drawBar (draw, 0xC000FF00, mX + 1 + mWidth*4/5, mY, (mWidth/5)-1, mHeight, mPower[5]);
+      drawBar (draw, 0x80FF0000, mX + 1,              mY,     mWidth-2, mHeight, mPower[3]);
       }
     }
   //}}}
 
 private:
   //{{{
-  void drawChanBar (iDraw* draw, int16_t x, int16_t y, uint16_t width, uint16_t height, float value) {
+  void drawBar (iDraw* draw, uint32_t colour, int16_t x, int16_t y, uint16_t width, uint16_t height, float value) {
 
-    y = y + height - int(value * kBarsScale * height);
-    draw->rectClipped (COL_GREEN, x, y, width, height-y);
-    }
-  //}}}
-  //{{{
-  void drawWooferBar (iDraw* draw, int16_t x, int16_t y, uint16_t width, uint16_t height, float value) {
-
-    y = y + height - int(value * kBarsScale * height);
-    draw->rectClipped (0x80FF0000, x, y, width, height-y);
+    y += height - int(value * kBarsScale * height);
+    draw->rectClipped (colour, x, y, width, height-y);
     }
   //}}}
 
