d972ce0d2b2906c3c9e8fb08f658dadfe7cde9eb
colinporth <colinwrey@hotmail.com>
2019-12-29 17:33:42 +0000

    fix multiple

--- utils/cFileList.h
+++ utils/cFileList.h
@@ -334,7 +334,7 @@ private:
       do {
         if ((findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && (findFileData.cFileName[0] != '.'))
           scanDirectory (pathFileName, findFileData.cFileName);
-        else if (PathMatchSpecA (findFileData.cFileName, mMatchString.c_str()))
+        else if (PathMatchSpecExA (findFileData.cFileName, mMatchString.c_str(), PMSF_MULTIPLE))
           if ((findFileData.cFileName[0] != '.') && (findFileData.cFileName[0] != '..'))
             mFileItemList.push_back (cFileItem (pathFileName, findFileData.cFileName));
         } while (FindNextFileA (file, &findFileData));
