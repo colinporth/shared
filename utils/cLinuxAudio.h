a3f506fa902aa838d0769faf8500acc78d249dc5
colinporth <colinwrey@hotmail.com>
2020-01-10 10:43:56 +0000

    Update cLinuxAudio.h

--- utils/cLinuxAudio.h
+++ utils/cLinuxAudio.h
@@ -13,52 +13,24 @@
 class cLinuxAudio : public iAudio {
 public:
   //{{{
-  cLinuxAudio() {
+  cLinuxAudio (int srcChannels, int srcSampleRate) {
+
     mSilence = (int16_t*)malloc (2048*4);
     memset (mSilence, 0, 2048*4);
+
+    open (srcChannels, srcSampleRate);
     }
   //}}}
   //{{{
   virtual ~cLinuxAudio() {
     free (mSilence);
-    }
-  //}}}
-
-  //{{{
-  void open (int srcChannels, int srcSampleRate) {
-
-    int err = snd_pcm_open (&mHandle, "default", SND_PCM_STREAM_PLAYBACK, 0);
-    if (err < 0)
-      printf ("audOpen - snd_pcm_open error: %s\n", snd_strerror (err));
-
-    err = snd_pcm_set_params (mHandle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 2, 48000, 1, 500000);
-    if (err < 0)
-      printf ("audOpen - snd_pcm_set_params  error: %s\n", snd_strerror(err));
-    }
-  //}}}
-  //{{{
-  void play (int srcChannels, int16_t* srcSamples, int srcNumSamples, float pitch) {
-
-    if (!srcSamples)
-      srcSamples = mSilence;
-
-    snd_pcm_sframes_t frames = snd_pcm_writei (mHandle, srcSamples, srcNumSamples);
-    if (frames < 0)
-      frames = snd_pcm_recover (mHandle, frames, 0);
-    if (frames < 0)
-      printf("audPlay - snd_pcm_writei failed: %s\n", snd_strerror(frames));
-    }
-  //}}}
-  //{{{
-  void close() {
-    snd_pcm_close (mHandle);
+    close();
     }
   //}}}
 
   int getDstChannels() { return 2; }
   int getDstSampleRate() { return 480000; }
   int getDstChannelMask() { return 0xFF; }
-
   int getSrcChannels() { return 2; }
 
   //{{{
@@ -88,9 +60,41 @@ public:
   eMixDown getMixDown() { return mMixDown; }
   void setMixDown (eMixDown mixDown) { mMixDown = mixDown; }
 
+  //{{{
+  void play (int srcChannels, int16_t* srcSamples, int srcNumSamples, float pitch) {
+
+    if (!srcSamples)
+      srcSamples = mSilence;
+
+    snd_pcm_sframes_t frames = snd_pcm_writei (mHandle, srcSamples, srcNumSamples);
+    if (frames < 0)
+      frames = snd_pcm_recover (mHandle, frames, 0);
+    if (frames < 0)
+      printf("audPlay - snd_pcm_writei failed: %s\n", snd_strerror(frames));
+    }
+  //}}}
+
   float mVolume = 0.8f;
 
 private:
+  //{{{
+  void open (int srcChannels, int srcSampleRate) {
+
+    int err = snd_pcm_open (&mHandle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+    if (err < 0)
+      printf ("audOpen - snd_pcm_open error: %s\n", snd_strerror (err));
+
+    err = snd_pcm_set_params (mHandle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 2, 48000, 1, 500000);
+    if (err < 0)
+      printf ("audOpen - snd_pcm_set_params  error: %s\n", snd_strerror(err));
+    }
+  //}}}
+  //{{{
+  void close() {
+    snd_pcm_close (mHandle);
+    }
+  //}}}
+
   snd_pcm_t* mHandle;
   int16_t* mSilence;
   eMixDown mMixDown = eBestMix;
