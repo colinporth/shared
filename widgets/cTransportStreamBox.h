eb58117090a9b37eba5dc4597abfe25eb5e0c307
colinporth <colinporth@users.noreply.github.com>
2020-07-08 11:11:37 +0100

    no height limit

--- widgets/cTransportStreamBox.h
+++ widgets/cTransportStreamBox.h
@@ -18,43 +18,37 @@ public:
   //}}}
   virtual ~cTransportStreamBox() {}
 
-  //{{{
   void onDraw (iDraw* draw) {
-
     auto y = mY+1;
     auto lineHeight = getFontHeight()*2/3;
 
-    if (mTs->mPidInfoMap.size()) {
-      for (auto &pidInfo : mTs->mPidInfoMap) {
-        mMaxPidPackets = std::max (mMaxPidPackets, pidInfo.second.mPackets);
-        auto frac = (float)pidInfo.second.mPackets / (float)mMaxPidPackets;
-        auto x = mX+2;
+    for (auto &pidInfo : mTs->mPidInfoMap) {
+      mMaxPidPackets = std::max (mMaxPidPackets, pidInfo.second.mPackets);
+      auto frac = (float)pidInfo.second.mPackets / (float)mMaxPidPackets;
+      auto x = mX+2;
 
-        auto str = dec (pidInfo.second.mPackets,mPacketDigits) +
-                   (mContDigits ? (":" + dec(pidInfo.second.mDisContinuity, mContDigits)) : "") +
-                   " " + dec(pidInfo.first, 4) +
-                   " " + getFullPtsString (pidInfo.second.mPts) +
-                   " " + pidInfo.second.getTypeString();
+      auto str = dec (pidInfo.second.mPackets,mPacketDigits) +
+                 (mContDigits ? (":" + dec(pidInfo.second.mDisContinuity, mContDigits)) : "") +
+                 " " + dec(pidInfo.first, 4) +
+                 " " + getFullPtsString (pidInfo.second.mPts) +
+                 " " + pidInfo.second.getTypeString();
 
-        auto width = draw->drawText (COL_LIGHTGREY, lineHeight, str,
-                                     x, y, mWidth-3, lineHeight);
-        draw->rectClipped (COL_DARKORANGE,
-                           x + width + lineHeight/2, y, int((mWidth - width) * frac), lineHeight - 1);
-        draw->drawText (COL_LIGHTGREY, lineHeight, pidInfo.second.getInfoString(),
-                        x + width + lineHeight/2, y, mWidth-width, lineHeight);
+      auto width = draw->drawText (COL_LIGHTGREY, lineHeight, str, x, y, mWidth-3, lineHeight);
+      draw->rectClipped (COL_DARKORANGE,
+                         x + width + lineHeight/2, y, int((mWidth - width) * frac), lineHeight - 1);
+      draw->drawText (COL_LIGHTGREY, lineHeight, pidInfo.second.getInfoString(),
+                      x + width + lineHeight/2, y, mWidth-width, lineHeight);
 
-        y += lineHeight;
-        if (y > mY + mHeight)
-          break;
+      if (pidInfo.second.mPackets > pow (10, mPacketDigits))
+        mPacketDigits++;
 
-        if (pidInfo.second.mPackets > pow (10, mPacketDigits))
-          mPacketDigits++;
-        }
-      if (mTs->getDiscontinuity() > pow (10, mContDigits))
-        mContDigits++;
+      y += lineHeight;
+      //if (y > mY + mHeight)
+      //  break;
       }
+    if (mTs->getDiscontinuity() > pow (10, mContDigits))
+      mContDigits++;
     }
-  //}}}
 
 private:
   cTransportStream* mTs;
