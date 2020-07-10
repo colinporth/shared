72345e532d96b9bd82d893a184321ea7a0d6efe2
colinporth <colinporth@users.noreply.github.com>
2020-07-10 18:21:25 +0100

    Update cVgGL.h

--- nanoVg/cVgGL.h
+++ nanoVg/cVgGL.h
@@ -749,7 +749,6 @@ private:
         "fpos = vertex;"
         "gl_Position = vec4(2.0*vertex.x/viewSize.x - 1.0, 1.0 - 2.0*vertex.y/viewSize.y, 0, 1);"
         "}";
-      "\n";
     //{{{
     const char* kFragShader =
       //  precision
