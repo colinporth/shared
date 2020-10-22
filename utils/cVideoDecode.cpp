386c4965d2bb6f7631275c017db2437d21d80ed1
colinporth <colinporth@users.noreply.github.com>
2020-10-22 11:27:50 +0100

    working planar arm neon

--- utils/cVideoDecode.cpp
+++ utils/cVideoDecode.cpp
@@ -647,22 +647,8 @@ void cVideoDecode::cFrame::set (int64_t pts, int64_t ptsDuration, int pesSize, i
 #else
   //{{{
   void cVideoDecode::cFrame::setYuv420RgbaPlanar (int width, int height, uint8_t** data, int* linesize) {
-  // ARM nenon covert, not quite working, seems slow to sws table lookup
+  // ARM NEON covert, maybe slower than sws table lookup
 
-    //{{{  convert to interleaved
-    //uint8_t* uvBuffer = (uint8_t*)malloc ((width/2) * (height/2) * 2);
-    //uint8_t* dst = uvBuffer;
-    //uint8_t* srcu = data[1];
-    //uint8_t* srcv = data[2];
-    //for (int y = 0; y < height/2; y++) {
-    //  for (int x = 0; x < width/2; x++) {
-    //    *dst++ = *srcu++;
-    //    *dst++ = *srcv++;
-    //    }
-    //  }
-    //decode_yuv_neon ((uint8_t*)mBuffer, yBuffer, uvBuffer, width, height, 0xFF);
-    //free (uvBuffer);
-    //}}}
     system_clock::time_point timePoint = system_clock::now();
 
     allocateBuffer (width, height);
@@ -673,75 +659,79 @@ void cVideoDecode::cFrame::set (int64_t pts, int64_t ptsDuration, int pesSize, i
     uint8_t* dst = (uint8_t*)mBuffer;
 
     // constants
-    int const stride = width * 4;
-    int const itHeight = height >> 1;
-    int const itWidth = width >> 3;
+    int const dstStride = width * 4;
+    int const heightDiv2 = height >> 1;
+    int const widthDiv8 = width >> 3;
 
-    uint8x8_t const Yshift = vdup_n_u8 (16);
-    int16x8_t const half = vdupq_n_u16 (128);
+    uint8x8_t const yOffset = vdup_n_u8 (16);
+    int16x8_t const uvOffset = vdupq_n_u16 (128);
     int32x4_t const rounding = vdupq_n_s32 (128);
 
     uint8x8x4_t block;
     block.val[3] = vdup_n_u8 (0xFF);
 
-    for (int j = 0; j < itHeight; ++j, y += width, dst += stride)
-      for (int i = 0; i < itWidth; ++i, y += 8, u += 4, v += 4, dst += (8 * 4)) {
-        //{{{  process 8pix with ARM NEON intrinsics, 2 rows at a time
-        // yrow0
-        uint16x8_t t = vmovl_u8 (vqsub_u8 (vld1_u8 (y), Yshift));
-        int32x4_t const Y00 = vmulq_n_u32 (vmovl_u16(vget_low_u16 (t)), 298);
-        int32x4_t const Y01 = vmulq_n_u32 (vmovl_u16(vget_high_u16 (t)), 298);
+    for (int j = 0; j < heightDiv2; ++j, y += width, dst += dstStride)
+      for (int i = 0; i < widthDiv8; ++i, y += 8, u += 4, v += 4, dst += (8 * 4)) {
+        //{{{  2 rows of 8 pix
+        // U load 8 u8 - u01234567 = u0 u1 u2 u3 u4 u5 u6 u7
+        uint8x8_t const u01234567 = vld1_u8 (u);
+        // - uu01234567 =  0.u0 0.u1 0.u2 0.u3 0.u4 0.u5 0.u6 0.u7
+        int16x8_t const uu01234567 = (int16x8_t)vmovl_u8 (u01234567);
+        // - u01234567o = (0.u0 0.u1 0.u2 0.u3 0.u4 0.u5 0.u6 0.u7) - half
+        int16x8_t const uu01234567o = vsubq_s16 (uu01234567, uvOffset);
+        // - u0123h = 0.u0 0.u1 0.u2 0.u3 - u4567 unused, unwind loop?
+        int16x4_t const uu0123o = vget_low_s16 (uu01234567o);
 
-        // yrow1
-        t = vmovl_u8 (vqsub_u8 (vld1_u8(y+width), Yshift));
-        int32x4_t const Y10 = vmulq_n_u32 (vmovl_u16 (vget_low_u16 (t)), 298);
-        int32x4_t const Y11 = vmulq_n_u32 (vmovl_u16 (vget_high_u16 (t)), 298);
+        // V load 8 u8 - v01234567 = v0 v1 v2 v3 v4 v5 v6 v7
+        uint8x8_t const v01234567 = vld1_u8 (v);
+        // - vv01234567 =  0.v0 0.v1 0.v2 0.v3 0.v4 0.v5 0.v6 0.v7
+        int16x8_t const vv01234567 = (int16x8_t)vmovl_u8 (v01234567);
+        // - vv01234567o = (0.v0 0.v1 0.v2 0.v3 0.v4 0.v5 0.v6 0.v7) - half
+        int16x8_t const vv01234567o = vsubq_s16 (vv01234567, uvOffset);
+        // - vv0123 = 0.v0 0.v1 0.v2 0.v3 - v4567 unused, unwind loop?
+        int16x4_t const vv0123o = vget_low_s16 (vv01234567o);
 
-        // interleaved
-        // loadvu pack 4 sets of uv into a uint8x8_t { v0,u0, v1,u1, v2,u2, v3,u3 }
-        // uint16x8_t t = vsubq_s16 ((int16x8_t)vmovl_u8 (vld1_u8 (uv)), half);
-        // UV.val[0] : v0, v1, v2, v3, UV.val[1] : u0, u1, u2, u3
-        // int16x4x2_t const UV = vuzp_s16 (vget_low_s16 (t), vget_high_s16 (t));
+        // r = 128 + 409v
+        int32x4_t const r0123os = vmlal_n_s16 (rounding, uu0123o, 409);
+        int32x4x2_t const r00112233os = vzipq_s32 (r0123os, r0123os);
+        // g = 128 - 100u - 208v
+        int32x4_t const g0123os = vmlal_n_s16 (vmlal_n_s16 (rounding, uu0123o, -208), vv0123o, -100);
+        int32x4x2_t const g00112233os = vzipq_s32 (g0123os, g0123os);
+        // b = 128 + 516u
+        int32x4_t const b0123os = vmlal_n_s16 (rounding, vv0123o, 516);
+        int32x4x2_t const b00112233os = vzipq_s32 (b0123os, b0123os);
 
-        // load 4 sets of u  u0 u1 u2 u3 u4 u5 u6 u7
-        //uint16x8_t const u0123 = vsubq_s16 ((int16x8_t)vmovl_u8 (vld1_u8 (u)), half);
-        uint16x8_t ldu = vld1_u8 (u);
-        uint16x8_t lduu = (int16x8_t)vmovl_u8 (ldu, half);
-        uint16x8_t const u0123 = vsubq_s16 (lduu);
+        // row0
+        uint8x8_t const row0y01234567 = vld1_u8 (y);
+        uint8x8_t const row0y01234567o = vqsub_u8 (row0y01234567, yOffset);
+        uint16x8_t const row0yy01234567o = vmovl_u8 (row0y01234567o);
 
-        //uint16x8_t const v0123 = vsubq_s16 ((int16x8_t)vmovl_u8 (vld1_u8 (v)), half);
-        uint16x8_t ldv = vld1_u8 (v);
-        uint16x8_t ldvv = (int16x8_t)vmovl_u8 (ldv, half);
-        uint16x8_t const v0123 = vsubq_s16 (ldvv);
+        int32x4_t const row0y0123os = vmulq_n_u32 (vmovl_u16 (vget_low_u16 (row0yy01234567o)), 298);
+        int32x4_t const row0y4567os = vmulq_n_u32 (vmovl_u16 (vget_high_u16 (row0yy01234567o)), 298);
 
-        int16x4x2_t const UV = vuzp_s16 (vget_low_s16 (u0123), vget_low_s16 (v0123));
-
-        // tR 128+409V, tG 128-100U-208V, tB 128+516U
-        int32x4_t const tR = vmlal_n_s16 (rounding, UV.val[0], 409);
-        int32x4_t const tG = vmlal_n_s16 (vmlal_n_s16 (rounding, UV.val[0], -208), UV.val[1], -100);
-        int32x4_t const tB = vmlal_n_s16 (rounding, UV.val[1], 516);
-
-        int32x4x2_t const R = vzipq_s32 (tR, tR); // [tR0, tR0, tR1, tR1] [ tR2, tR2, tR3, tR3]
-        int32x4x2_t const G = vzipq_s32 (tG, tG); // [tG0, tG0, tG1, tG1] [ tG2, tG2, tG3, tG3]
-        int32x4x2_t const B = vzipq_s32 (tB, tB); // [tB0, tB0, tB1, tB1] [ tB2, tB2, tB3, tB3]
-
-        // upper 8 pixels
-        block.val[0] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (B.val[0], Y00)),
-                                                  vqmovun_s32 (vaddq_s32 (B.val[1], Y01))), 8);
-        block.val[1] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (G.val[0], Y00)),
-                                                  vqmovun_s32 (vaddq_s32 (G.val[1], Y01))), 8),
-        block.val[2] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (R.val[0], Y00)),
-                                                  vqmovun_s32 (vaddq_s32 (R.val[1], Y01))), 8),
+        block.val[0] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (b00112233os.val[0], row0y0123os)),
+                                                  vqmovun_s32 (vaddq_s32 (b00112233os.val[1], row0y4567os))), 8);
+        block.val[1] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (g00112233os.val[0], row0y0123os)),
+                                                  vqmovun_s32 (vaddq_s32 (g00112233os.val[1], row0y4567os))), 8),
+        block.val[2] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (r00112233os.val[0], row0y0123os)),
+                                                  vqmovun_s32 (vaddq_s32 (r00112233os.val[1], row0y4567os))), 8),
         vst4_u8 (dst, block);
 
-        // lower 8 pixels
-        block.val[0] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (B.val[0], Y10)),
-                                                  vqmovun_s32 (vaddq_s32 (B.val[1], Y11))), 8);
-        block.val[1] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (G.val[0], Y10)),
-                                                  vqmovun_s32 (vaddq_s32 (G.val[1], Y11))), 8),
-        block.val[2] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (R.val[0], Y10)),
-                                                  vqmovun_s32 (vaddq_s32 (R.val[1], Y11))), 8),
-        vst4_u8 (dst+stride, block);
+        // row1
+        uint8x8_t const row1y01234567 = vld1_u8 (y+width);
+        uint8x8_t const row1y01234567o = vqsub_u8 (row1y01234567, yOffset);
+        uint16x8_t const row1yy01234567o = vmovl_u8 (row1y01234567o);
+
+        int32x4_t const row1y0123os = vmulq_n_u32 (vmovl_u16 (vget_low_u16 (row1yy01234567o)), 298);
+        int32x4_t const row1y4567os = vmulq_n_u32 (vmovl_u16 (vget_high_u16 (row1yy01234567o)), 298);
+
+        block.val[0] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (b00112233os.val[0], row1y0123os)),
+                                                  vqmovun_s32 (vaddq_s32 (b00112233os.val[1], row1y4567os))), 8);
+        block.val[1] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (g00112233os.val[0], row1y0123os)),
+                                                  vqmovun_s32 (vaddq_s32 (g00112233os.val[1], row1y4567os))), 8),
+        block.val[2] = vshrn_n_u16 (vcombine_u16 (vqmovun_s32 (vaddq_s32 (r00112233os.val[0], row1y0123os)),
+                                                  vqmovun_s32 (vaddq_s32 (r00112233os.val[1], row1y4567os))), 8),
+        vst4_u8 (dst + dstStride, block);
         }
         //}}}
 
