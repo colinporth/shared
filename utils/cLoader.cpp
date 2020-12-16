8132e47fa76cb979bd17352c9470810d7fcf0f5a
colinporth <colinporth@users.noreply.github.com>
2020-12-16 10:03:52 +0000

    Update cLoader.cpp

--- utils/cLoader.cpp
+++ utils/cLoader.cpp
@@ -1893,15 +1893,9 @@ public:
       }
       //}}}
 
-    //{{{  allow multiple sockets to use the same PORT number
-    //char yes = 1;
-    //if (setsockopt (rtpReceiveSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&yes, sizeof(yes)) != 0) {
-      //{{{  error return
-      //cLog::log (LOGERROR, "socket setsockopt SO_REUSEADDR failed");
-      //return;
-      //}
-      //}}}
-    //}}}
+    // allow multiple sockets to use the same PORT number, no error check?
+    char yes = 1;
+    setsockopt (rtpReceiveSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&yes, sizeof(yes));
 
     // bind the socket to anyAddress:specifiedPort
     struct sockaddr_in recvAddr;
